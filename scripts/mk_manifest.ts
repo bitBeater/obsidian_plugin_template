4cee89339407d492c6008390baaf125f4cbed6b6
alexrr2iggs <alexandru.bunduche@yahoo.com>
2024-04-24 18:56:04 +0200

    add manifest funding parsing

--- scripts/mk_manifest.ts
+++ scripts/mk_manifest.ts
@@ -3,6 +3,24 @@ import { log } from 'npm:iggs-utils@1.3.1';
 import packageJson from '../package.json' with { type: 'json' };
 import { MANIFEST } from './utils.ts';
 
+/**
+ * @link see docs https://docs.npmjs.com/cli/v10/configuring-npm/package-json#funding
+ */
+type PackageJsonFunding = string | { type: string, url: string } | PackageJsonFunding[];
+
+/**
+ * @link see docs https://docs.obsidian.md/Reference/Manifest#fundingUrl
+ */
+type ObsidianManifestFunding = { [key: string]: string };
+
+function parseFunding(funding: PackageJsonFunding): ObsidianManifestFunding {
+    if (typeof funding === 'string') return { [new URL(funding).hostname]: funding };
+    if (Array.isArray(funding)) return funding.reduce((acc, f) => ({ ...acc, ...parseFunding(f) }), {});
+    if (typeof funding === 'object') return { [funding.type]: funding.url };
+    throw new Error('Invalid funding type', funding);
+}
+
+// ----------------------------------------------------------------------------------------------
 
 const manifeStep = new log.Logger({ logLevel: log.LogLevel.INFO }).step('[MK_MANIFEST]');
 
@@ -15,7 +33,7 @@ const manifest = {
     description: packageJson?.description,
     author: packageJson?.author?.name || packageJson?.author,
     authorUrl: packageJson?.author?.url,
-    fundingUrl: packageJson?.funding?.url,
+    fundingUrl: parseFunding(packageJson?.funding),
     ...packageJson.manifest,
 }
 
